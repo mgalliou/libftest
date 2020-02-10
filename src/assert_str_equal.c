b65802628792372cd64046e757aff771edc78bba
mgalliou <matthias.galliou@gmail.com>
2020-02-10 17:58:38 +0100

    improve assert_str_equal output

--- src/assert_str_equal.c
+++ src/assert_str_equal.c
@@ -6,7 +6,7 @@
 /*   By: mgalliou <mgalliou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/09 17:27:31 by mgalliou          #+#    #+#             */
-/*   Updated: 2019/10/25 13:26:02 by mgalliou         ###   ########.fr       */
+/*   Updated: 2020/02/10 17:56:55 by mgalliou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,20 @@
 
 void 	assert_str_equal(char *expected, char *actually)
 {
-	if (strcmp(actually, expected))
+	char *null = "(null)";
+
+	if ((!expected && actually) ||
+			(expected && !actually) ||
+			strcmp(expected, actually))
 	{
+		if (!expected)
+		{
+			expected = null;
+		}
+		if (!actually)
+		{
+			actually = null;
+		}
 		dprintf(2, "test: %s: failed\n", get_test_mng()->current_test);
 		dprintf(2, "expected: \"%s\"\nactually: \"%s\".\n", expected, actually);
 		set_cur_test_ret(EXIT_FAILURE);
